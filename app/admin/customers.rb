0dc92f51b45de658e6db9ea9f09511cad9c1693c
lhunter50 <lhunter50@academic.rrc.ca>
2020-11-04 12:49:46 -0800

    initial commit

---
+++ app/admin/customers.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register Customer do
+  permit_params :fullname, :phonenumber, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+
+  end
+
+end
